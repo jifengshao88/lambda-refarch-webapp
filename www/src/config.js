16b5fd8619d257dec6190fa5eb6b4c6c1b08e497
Shao <Jifeng-Shao@mercer.com>
2020-07-05 01:53:38 -0500

    updated

--- www/src/config.js
+++ www/src/config.js
@@ -2,12 +2,12 @@
 // aws cloudformation describe-stacks --stack-name <YOUR STACK NAME> --query "Stacks[0].Outputs[]"
 
 const config = {
-  "aws_cognito_region": "AWS_COGNITO_REGION",
-  "aws_user_pools_id": "AWS_USER_POOLS_ID",                           // CognitoID
-  "aws_user_pools_web_client_id": "AWS_USER_POOLS_WEB_CLIENT_ID",     // CognitoClientID
-  "api_base_url": "API_BASE_URL",                                     // TodoFunctionApi
-  "coginto_hosted_domain": "COGNITO_HOSTED_DOMAIN",                   // CognitoDomainName
-  "redirect_url": "REDIRECT_URL"                                      // AmplifyURL
+  "aws_cognito_region": "us-east-2",
+  "aws_user_pools_id": "us-east-2_yzKH2mvED",                           // CognitoID
+  "aws_user_pools_web_client_id": "5i057dvtlegpr9cdfu3lhmum1q",     // CognitoClientID
+  "api_base_url": "https://q8diu2g9db.execute-api.us-east-2.amazonaws.com/prod",                                     // TodoFunctionApi
+  "coginto_hosted_domain": "mytodoappdemo-pt-admin-sam.auth.us-east-2.amazoncognito.com",                   // CognitoDomainName
+  "redirect_url": "https://amplify-console.d1t2n5zld5m1yo.amplifyapp.com"                                      // AmplifyURL
 };
 
 export default config;
