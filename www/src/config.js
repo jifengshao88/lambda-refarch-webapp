cb7c80791892f1dd6455da1fcc726be391ea796f
Shao <Jifeng-Shao@mercer.com>
2020-07-05 03:00:53 -0500

    updated

--- www/src/config.js
+++ www/src/config.js
@@ -3,11 +3,11 @@
 
 const config = {
   "aws_cognito_region": "us-east-2",
-  "aws_user_pools_id": "us-east-2_yzKH2mvED",                           // CognitoID
+  "aws_user_pools_id": "us-east-2_NgjJXSMbN",                           // CognitoID
   "aws_user_pools_web_client_id": "5i057dvtlegpr9cdfu3lhmum1q",     // CognitoClientID
-  "api_base_url": "https://q8diu2g9db.execute-api.us-east-2.amazonaws.com/prod",                  // TodoFunctionApi
-  "coginto_hosted_domain": "mytodoappdemo-pt-admin-sam.auth.us-east-2.amazoncognito.com",                   // CognitoDomainName
-  "redirect_url": "https://amplify-console.d1t2n5zld5m1yo.amplifyapp.com"                                      // AmplifyURL
+  "api_base_url": "https://7zfczic967.execute-api.us-east-2.amazonaws.com/prod",              // TodoFunctionApi
+  "coginto_hosted_domain": "mytodoappdemo-pt-admin-sam.auth.us-east-2.amazoncognito.com",    // CognitoDomainName
+  "redirect_url": "https://master.d1122pb0dtr9yw.amplifyapp.com"                    // AmplifyURL
 };
 
 export default config;
