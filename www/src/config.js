b910299c34cc38a83eff9b50db24bfdd6411dc16
Shao <Jifeng-Shao@mercer.com>
2020-07-05 03:33:50 -0500

    updated

--- www/src/config.js
+++ www/src/config.js
@@ -3,11 +3,11 @@
 
 const config = {
   "aws_cognito_region": "us-east-2",
-  "aws_user_pools_id": "us-east-2_NgjJXSMbN",                           // CognitoID
-  "aws_user_pools_web_client_id": "5i057dvtlegpr9cdfu3lhmum1q",     // CognitoClientID
-  "api_base_url": "https://7zfczic967.execute-api.us-east-2.amazonaws.com/prod",              // TodoFunctionApi
-  "coginto_hosted_domain": "mytodoappdemo-pt-admin-sam.auth.us-east-2.amazoncognito.com",    // CognitoDomainName
-  "redirect_url": "https://master.d1122pb0dtr9yw.amplifyapp.com"                    // AmplifyURL
+  "aws_user_pools_id": "us-east-2_D5MKE76CW",                           // CognitoID
+  "aws_user_pools_web_client_id": "1cl9116f38vqtlqpofll7om788",     // CognitoClientID
+  "api_base_url": "https://qdplfih4s2.execute-api.us-east-2.amazonaws.com/prod",    // TodoFunctionApi
+  "coginto_hosted_domain": "mytodoappdemo-pt-admin-sam.auth.us-east-2.amazoncognito.com",  // CognitoDomainName
+  "redirect_url": "https://master.d16m7bpnqn7qi1.amplifyapp.com"                    // AmplifyURL
 };
 
 export default config;
